b6da32d60fdafb44b4cd7f6dd17cd442b2df9390
victoriaUNI <victoria.00000851663@unicap.br>
2025-03-19 02:06:24 +0000

    .

---
+++ card/App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/logo.jpg')} style={styles.image} />
+      <Text style={styles.name}>killer queen almeida medeiros (kira)</Text>
+      <Text style={styles.description}>Bebezinho da victoria.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 20,
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+    paddingHorizontal: 20,
+  },
+});
